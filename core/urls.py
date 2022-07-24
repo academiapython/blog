a6096b46ef6f1fa359a8c2993d8ee00a3e350e20
academiapython <academiapython.py@gmail.com>
2022-07-23 23:03:13 -0300

    # Utilizando sistema de alteração de senhas do Django - Parte I.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 
-
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('exemplos/', include('exemplos.urls')),
-    path('login/', include('autenticacao.urls')),
+    path('contas/', include('autenticacao.urls')),
     path('contato/', include('contato.urls')),
     path('cursos/', include('cursos.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
